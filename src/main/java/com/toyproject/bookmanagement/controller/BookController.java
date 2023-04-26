a2b9bc928d4afb950ec181d9f266ba9521a703b6
jw0302 <wkdwlsdnjs41@naver.com>
2023-04-26 17:51:00 +0900

    Spring Boot book_management 20230426 - 사이트에서 book 클릭 시 해당 book 페이지 이동 -get 요청 bookId - 책 좋아요 기능 추가 -get 요청 map으로 받아서 좋아요 클릭한 userId 찻도록 코드 작성

--- src/main/java/com/toyproject/bookmanagement/controller/BookController.java
+++ src/main/java/com/toyproject/bookmanagement/controller/BookController.java
@@ -2,6 +2,7 @@ package com.toyproject.bookmanagement.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.toyproject.bookmanagement.dto.book.SearchBookReqDto;
@@ -14,6 +15,12 @@ import lombok.RequiredArgsConstructor;
 public class BookController {
 	
 	private final BookService bookService;
+	
+	
+	@GetMapping("/book/{bookId}")
+	public ResponseEntity<?> getBook(@PathVariable int bookId) {
+		return ResponseEntity.ok().body(bookService.getBook(bookId));
+	}
 
 	
 	@GetMapping("/books")
@@ -26,5 +33,11 @@ public class BookController {
 	@GetMapping("/categories")
 	public ResponseEntity<?> categories() {
 		return ResponseEntity.ok().body(bookService.getCategories());
+	}
+	
+	
+	@GetMapping("/book/{bookId}/like")
+	public ResponseEntity<?> getLikeCount(@PathVariable int bookId) {
+		return ResponseEntity.ok().body(bookService.getLikeCount(bookId));
 	}
 }
--- src/main/java/com/toyproject/bookmanagement/entity/Book.java
+++ src/main/java/com/toyproject/bookmanagement/entity/Book.java
@@ -1,5 +1,6 @@
 package com.toyproject.bookmanagement.entity;
 
+import com.toyproject.bookmanagement.dto.book.GetBookRespDto;
 import com.toyproject.bookmanagement.dto.book.SearchBookRespDto;
 
 import lombok.AllArgsConstructor;
@@ -19,6 +20,7 @@ public class Book {
 	private int publisherId;
 	private int categoryId;
 	private String coverImgUrl;
+	private int likeCount;
 	
 	private Author author;
 	private Publisher publisher;
@@ -33,6 +35,18 @@ public class Book {
 				.publisherId(publisherId)
 				.publisherName(publisher.getPublisherName())
 				.categoryId(categoryId)
+				.categoryName(category.getCategoryName())
+				.coverImgUrl(coverImgUrl)
+				.likeCount(likeCount)
+				.build();
+	}
+	
+	public GetBookRespDto toGetBookDto() {
+		return GetBookRespDto.builder()
+				.bookId(bookId)
+				.bookName(bookName)
+				.authorName(author.getAuthorName())
+				.publisherName(publisher.getPublisherName())
 				.categoryName(category.getCategoryName())
 				.coverImgUrl(coverImgUrl)
 				.build();
