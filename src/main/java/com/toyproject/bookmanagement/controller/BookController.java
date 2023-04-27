b9fa487f70ffdd7566068bbb3363cb060f8117e8
jw0302 <wkdwlsdnjs41@naver.com>
2023-04-27 16:45:26 +0900

    내가 만든 대여하기/반납하기 기능

--- src/main/java/com/toyproject/bookmanagement/controller/BookController.java
+++ src/main/java/com/toyproject/bookmanagement/controller/BookController.java
@@ -1,8 +1,14 @@
 package com.toyproject.bookmanagement.controller;
 
+import java.util.Map;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.toyproject.bookmanagement.dto.book.SearchBookReqDto;
@@ -40,4 +46,57 @@ public class BookController {
 	public ResponseEntity<?> getLikeCount(@PathVariable int bookId) {
 		return ResponseEntity.ok().body(bookService.getLikeCount(bookId));
 	}
+	
+	
+	@GetMapping("/book/{bookId}/like/status")
+	public ResponseEntity<?> getLikeStatus(@PathVariable int bookId, @RequestParam int userId) {
+		return ResponseEntity.ok().body(bookService.getLikeStatus(bookId, userId));
+	}
+	
+	
+	@PostMapping("/book/{bookId}/like")
+	public ResponseEntity<?> setLike(@PathVariable int bookId, @RequestBody Map<String, Integer> requestMap) {
+		return ResponseEntity.ok().body(bookService.setLike(bookId, requestMap.get("userId")));
+	}
+	
+	
+	@DeleteMapping("/book/{bookId}/like")
+	public ResponseEntity<?> disLike(@PathVariable int bookId, int userId) {
+		return ResponseEntity.ok().body(bookService.disLike(bookId, userId));
+	}
+	
+	
+	@GetMapping("/book/{bookId}/rental/list")
+	public ResponseEntity<?> getRentalListByBookId(@PathVariable int bookId) {
+		return ResponseEntity.ok().body(bookService.getRentalListByBookId(bookId));
+	}
+	
+	
+	@GetMapping("/book/{bookId}/rental")
+	public ResponseEntity<?> getRentalCount(@PathVariable int bookId) {
+		return ResponseEntity.ok().body(bookService.getRentalCount(bookId));
+	}
+	
+	
+	@GetMapping("/book/{bookId}/rental/status")
+	public ResponseEntity<?> getRentalStatus(@PathVariable int bookId, @RequestParam int userId) {
+		return ResponseEntity.ok().body(bookService.getRentalStatus(bookId, userId));
+	}
+	
+	
+	@PostMapping("/book/{bookId}/rental")
+	public ResponseEntity<?> rentalBook(@PathVariable int bookId, @RequestBody Map<String, Integer> requestMap) {
+		return ResponseEntity.ok().body(bookService.rentalBook(bookId, requestMap.get("userId")));
+	}
+	
+	
+	@DeleteMapping("/book/{bookId}/rental")
+	public ResponseEntity<?> returnBook(@PathVariable int bookId, int userId) {
+		return ResponseEntity.ok().body(bookService.returnBook(bookId, userId));
+	}
+	
+	
+	
+	
+	
 }
--- src/main/java/com/toyproject/bookmanagement/repository/BookRepository.java
+++ src/main/java/com/toyproject/bookmanagement/repository/BookRepository.java
@@ -7,17 +7,30 @@ import org.apache.ibatis.annotations.Mapper;
 
 import com.toyproject.bookmanagement.entity.Book;
 import com.toyproject.bookmanagement.entity.Category;
+import com.toyproject.bookmanagement.entity.RentalList;
 
 @Mapper
 public interface BookRepository {
 	
 	public Book getBook(int bookId);
 
+	// 도서 검색 & 전체갯수
 	public List<Book> searchBooks(Map<String, Object> map);
 	public int getTotalCount(Map<String, Object> map);
 	
+	// 카테고리 가져오기
 	public List<Category> getCategories();
 	
+	// 좋아요 기능
 	public int getLikeCount(int bookId);
 	public int getLikeStatus(Map<String, Object> map);
+	public int setLike(Map<String, Object> map);
+	public int disLike(Map<String, Object> map);
+	
+	// 대여 기능
+	public List<RentalList> getRentalListByBookId(int bookId);
+	public int getRentalCount(int bookId);
+	public int getRentalStatus(Map<String, Object> map);
+	public int rentalBook(Map<String, Object> map);
+	public int returnBook(Map<String, Object> map);
 }
